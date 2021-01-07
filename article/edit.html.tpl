ee263a4ae4530b77913dfbe6f368ee9ca6f9f778
mrmtsu <81202muramatsu@gmail.com>
2021-01-07 19:16:45 +0900

    routing

---
+++ article/edit.html.tpl
@@ -0,0 +1,10 @@
+{{define "hello"}}
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>{{.Title}} </h1>
+    <p>{{.Text}}</p>
+    <p>{{.ID}}</p>
+  </body>
+</html>
+{{end}}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
@@ -17,6 +18,7 @@ type Template struct {
 type Message struct {
 	Title string
 	Text  string
+	ID    int
 }
 
 // Render ...
@@ -24,18 +26,57 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-// Hello ...
-func Hello(c echo.Context) error {
-	m := &Message{
-		Title: "Hello world",
-		Text:  "hogehoge",
+// articleIndex ...
+func articleIndex(c echo.Context) error {
+	data := &Message{
+		Title: "Article Index",
+		Text:  "index!",
 	}
-	return c.Render(http.StatusOK, "hello", m)
+	return c.Render(http.StatusOK, "article/index.html.tpl", data)
+}
+
+// articleNew
+func articleNew(c echo.Context) error {
+	data := &Message{
+		Title: "Article New",
+		Text:  "New!",
+	}
+
+	return c.Render(http.StatusOK, "article/new.html.tpl", data)
+}
+
+// articleShow
+func articleShow(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := &Message{
+		Title: "Article Show",
+		Text:  "Show!",
+		ID:    id,
+	}
+
+	return c.Render(http.StatusOK, "article/show.html.tpl", data)
+}
+
+// articleEdit ...
+func articleEdit(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := &Message{
+		Title: "Article Edit",
+		Text:  "Edit!",
+		ID:    id,
+	}
+
+	return c.Render(http.StatusOK, "article/edit.html.tpl", data)
 }
 
 func main() {
 	e := echo.New()
-	e.GET("/hello", Hello)
+	e.GET("/hello", articleIndex)
+	e.GET("/new", articleNew)
+	e.GET("/:id", articleShow)
+	e.GET("/edit/:id", articleEdit)
 
 	t := &Template{
 		templates: template.Must(template.ParseGlob("./*.tpl")),
